219c719cf52419d2cce58b9fac9a34d7312738d3
RelebogileChiloane <167516379+RelebogileChiloane@users.noreply.github.com>
2024-11-16 13:39:52 +0200

    main

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
-// server.js
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
 
 server.listen(3000, () => {
   console.log('Server listening on http://localhost:3000');
 });
+const { MongoClient } = require("mongodb");
+const { throwDeprecation } = require('process');
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+    }
+}
+connectDB();
